2f8e12cc93a006c110ac21e7fcf5b438ecbe2380
NEJI <mrkochetov@yandex.ru>
2023-06-05 11:49:22 +0300

    new3

--- codewars/7kyu/7kyu.py
+++ codewars/7kyu/7kyu.py
@@ -1,9 +1,39 @@
 # Odd or Even?
-# def odd_or_even(arr):
-#     s = sum(arr)
-#     if s%2==0:
-#         return "even"
-#     else:
-#         return "odd"
-#
-# print(odd_or_even([0, 1, 2]))
+def odd_or_even(arr):
+    s = sum(arr)
+    if s%2==0:
+        return "even"
+    else:
+        return "odd"
+
+
+print(odd_or_even([0, 1, 2]))
+
+
+# String ends with?
+def solution(text, ending):
+    otvet = text[-len(ending):]
+    if ending == otvet:
+        return True
+    return False
+
+
+print(solution('abc', 'bc'))
+print(solution("fails",   "ails"))
+print(solution("sumo",    "omo"))
+
+
+def reverse_words(s):
+    if s == '':
+        return ''
+    for x in range(len(s)):
+        return ' '.join([x[::-1] for x in s.split(' ')])
+
+
+print(reverse_words(''))
+print(reverse_words('double  spaced  words'))
+
+
+
+
+
